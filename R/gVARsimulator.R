f95553141ee134745d612fca8d106ab559c3df31
SimonStuber <simon.stuber1@gmail.com>
2016-04-15 15:44:15 +0200

    Added files via upload
    Skewness

--- R/gVARsimulator.R
+++ R/gVARsimulator.R
@@ -38,7 +38,7 @@ randomGVARmodel <- function(
       evB <- eigen(trueBeta)$values
     }
     
-    
+
     if (all(evK > 0) & all(Re(evB)^2 + Im(evB)^2 < 1)){
       break
     }
@@ -88,10 +88,10 @@ graphicalVARsim <- function(
   
   Sigma <- solve(kappa)
   
-  #   lbound <- (lbound - mean) / sd
-  #   ubound <- (ubound - mean) / sd
-  #   
-  
+#   lbound <- (lbound - mean) / sd
+#   ubound <- (ubound - mean) / sd
+#   
+
   if (sum(skewed==rep(0,ncol(kappa)))==ncol(kappa)){
     for (t in 2:totTime){
       Data[t,] <- t(beta %*% Data[t-1,])  + rmvnorm(1, rep(0,Nvar), Sigma)
@@ -100,8 +100,7 @@ graphicalVARsim <- function(
     }
   }else{
     for (t in 2:totTime){#Needed to round Omega to avoid error "not symmetrical"
-      Data[t,] <- t(beta %*% Data[t-1,])  + sn::rmsn(n=1,dim=1*Nvar, mu=rep(0,Nvar),Omega=round(Sigma,digits = 5),
-                                                     alpha=skewed)[1,]
+      Data[t,] <- t(beta %*% Data[t-1,])  + rmsn(1,rep(0,Nvar),Sigma,skewed)[1,]
       Data[t,] <- ifelse(Data[t,]  < lbound, lbound, Data[t,] )
       Data[t,] <- ifelse(Data[t,]  > ubound, ubound, Data[t,] )
     }
@@ -111,7 +110,7 @@ graphicalVARsim <- function(
       Data[t,] <- Data[t,] +  rmvnorm(1, rep(0,Nvar), Sigma)
     }
   }
-  #,alpha=rnorm(n = Nvar,mean = 0,sd = 7)
+#,alpha=rnorm(n = Nvar,mean = 0,sd = 7)
   
   return(Data[-seq_len(warmup), ,drop=FALSE])
 }
