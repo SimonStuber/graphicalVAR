73e973dba2af56c44690ed448856bebc6aaef893
SimonStuber <simon.stuber1@gmail.com>
2016-05-12 15:06:31 +0200

    Add files via upload

--- R/gVARsimulator.R
+++ R/gVARsimulator.R
@@ -100,7 +100,7 @@ graphicalVARsim <- function(
   
   if (skewed){
     for (t in 2:totTime){
-      Data[t,] <- t(beta %*% Data[t-1,])  + skewDat[t,]
+      Data[t,] <- t(beta %*% Data[t-1,])  + skewDat[t-1,]
       Data[t,] <- ifelse(Data[t,]  < lbound, lbound, Data[t,] )
       Data[t,] <- ifelse(Data[t,]  > ubound, ubound, Data[t,] )
     }
